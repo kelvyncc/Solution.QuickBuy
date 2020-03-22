84c2b34790906412969f3092d0533fdbe46ce729
kelvyn cardoso <kelvyn.cardoso@hotmail.com>
2020-03-22 18:07:28 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,31 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+
+        //PEDIDO DEVER TER PELO MENOS UM ITENS DE PEDIDO OU MUITOS ITENS DE PEDIDOS
+        public ICollection<ItemPedido> ItensPedidos { get; set; }
+
+
+
+    }
+}
---
+++ QuickBuy.Dominio/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace QuickBuy.Dominio.Enumerados
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido = 0,
+        Boleto = 1,
+        CartãoDeCredito = 2,
+        Deposito = 3
+
+    }
+}
