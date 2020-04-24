c70dd9711ce95cc982176cce4f2e916e11ffb07f
kelvyn cardoso <kelvyn.cardoso@hotmail.com>
2020-04-24 14:59:48 -0300

    navegação das paginas

---
+++ QuickBuy.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Entidades;
+using System;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost("VerificarUsuario")]
+        public ActionResult VerificarUsuario([FromBody]Usuario usuario)
+        {
+            try
+            {
+                if(usuario.Email=="kelvyncc@gmail.com" && usuario.Senha == "abc123")                
+                    return Ok(usuario);
+                
+                return BadRequest("Usuario ou senha invalido ");
+
+               
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            try
+            {
+                return Ok();
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+
+    }
+}
